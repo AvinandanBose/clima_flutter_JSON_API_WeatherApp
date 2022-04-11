e4274c863e13df16f9a67c58d0ec4a3253603038
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-04-11 17:05:58 +0530

    Add files via upload

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -25,7 +25,9 @@ class _CityScreenState extends State<CityScreen> {
               Align(
                 alignment: Alignment.topLeft,
                 child: FlatButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                   child: const Icon(
                     Icons.arrow_back_ios,
                     size: 50.0,
